3800af6fa1eabde2a602127322544bdf318b34d8
mattmuroya <matt.muroya@gmail.com>
2022-06-27 11:12:21 -1000

    add comments to utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -3,10 +3,18 @@ import { PostDatum } from "../types/types";
 import fs from "fs";
 import matter from "gray-matter";
 
+// fetches an array of file names from the file system
 export const getFileNames = (): string[] => {
   return fs.readdirSync("posts");
 };
 
+// converts the array of file names to an array of post data
+// [
+//   {
+//     slug: string,
+//     frontmatter: {title: string, date: string},
+//   };
+// ]
 export const getPostData = (fileNames: string[]): PostDatum[] => {
   return fileNames.map((fileName) => {
     const slug = fileName.replace(/\.md$/, "");
@@ -19,6 +27,14 @@ export const getPostData = (fileNames: string[]): PostDatum[] => {
   });
 };
 
+// converts the array of file names to an array of param objects with a URI path (slug):
+// [
+//   {
+//     params: {
+//       slug: string
+//     },
+//   };
+// ]
 export const getPossiblePaths = (
   fileNames: string[]
 ): { params: { slug: string } }[] => {
@@ -31,6 +47,7 @@ export const getPossiblePaths = (
   });
 };
 
+// returns the contents of a blog post document as a string
 export const getFileContents = (params: any): string => {
   return fs.readFileSync(`posts/${params.slug}.md`, "utf-8");
 };
