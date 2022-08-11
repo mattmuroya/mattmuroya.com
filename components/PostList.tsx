15adb5c160964720378ace4b57efc446b28b3b0b
mattmuroya <matt.muroya@gmail.com>
2022-08-10 21:24:12 -1000

    extract post list styles

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
+import styles from "../styles/PostList.module.css";
 import { PostDatum } from "../types/types";
 
 export default function PostsList({ postData }: { postData: PostDatum[] }) {
   return (
     <section>
       <h2>Here&apos;s what&apos;s on my mind.</h2>
-      <ul
-        style={{ marginTop: "2rem", listStyleType: "none", paddingLeft: "0" }}
-      >
+      <ul className={styles.postList}>
         {postData.map((post) => (
           <li key={post.slug}>
             <Link href={`/posts/${post.slug}`}>
               <a>{post.frontmatter.title}</a>
-            </Link>{" "}
-            <span style={{ color: "#999999" }}>{post.frontmatter.date}</span>
+            </Link>
+            <br />
+            <span className={styles.date}>{post.frontmatter.date}</span>
           </li>
         ))}
       </ul>
